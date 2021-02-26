9943bde262c465fd704207d1760cd18620087c82
gmaheshwari2006 <32501070+gmaheshwari2006@users.noreply.github.com>
2021-02-26 18:59:48 -0500

    changes in gitignore for build and web3client file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import web3 from './web3Client';
+//import ethereum from './web3Client';
 
-function App() {
+ function App() {
+  console.log("web3 version: " + web3.version);
+  web3.eth.getAccounts().then(accts => console.log("accts are: " + accts));
+  //const accounts = await ethereum.request({ method: 'eth_accounts' });
+  //console.log("eth accounts: " + accounts);
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
---
+++ src/web3Client.js
@@ -0,0 +1,8 @@
+import Web3 from 'web3';
+
+
+const web3 = new Web3(window.web3.currentProvider);
+//const ethereum = new Web3(window.etherum);
+
+export default web3;
+//export default ethereum;
